5506978313b93eb88cab3ead8fb8a6848d2d5368
Héctor Villalobos <h.villalobosm03@gmail.com>
2021-07-06 15:46:59 -0400

    trabajando en el header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,34 @@
 <template>
-    <h1>hola mundo</h1>
+    
+  <!--navbar-->
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+   <div class="container">
+     <a class="navbar-brand" href="index.html">
+   <img src="../assets/img/puente_cuantico_final.png" width="110" height="80" alt="" loading="lazy">
+    </a>
+     <a class="navbar-brand" href="index.html">Puente Cuántico</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+ <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNav">
+ <ul class="navbar-nav ml-auto">
+   <li class="nav-item active">
+     <a class="nav-link" href="index.html">Inicio <span class="sr-only">(current)</span></a>
+   </li>
+   <li class="nav-item">
+     <a class="nav-link" href="numerologia.html">Numerología Universal</a>
+   </li>
+   <li class="nav-item">
+     <a class="nav-link" href="#">Pricing</a>
+   </li>
+   <li class="nav-item">
+     <a class="nav-link" href="#">Disabled</a>
+   </li>
+ </ul>
+</div>
+</div>
+</nav>
 </template>
 
 <script>
