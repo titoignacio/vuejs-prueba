97da4c7072f98e15b4f3c6b4331810e944a07dc0
Héctor Villalobos <h.villalobosm03@gmail.com>
2021-07-22 18:19:02 -0400

    margenes al carusel

--- src/components/Carusel.vue
+++ src/components/Carusel.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="container" id="caruselid">
 <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
