d6f64e4cc4b4c88eb7a776226addb90143246e38
Héctor Villalobos <h.villalobosm03@gmail.com>
2021-07-23 17:41:44 -0400

    articulos y videos hechos

--- src/components/Contenido.vue
+++ src/components/Contenido.vue
@@ -2,10 +2,30 @@
     <div class="container">
   <div class="row">
     <div class="col">
-      1 of 2
+     <span class="badge badge-pill badge-dark" id="articulos">Articulos</span>
+
+        <!-- card -->
+
+        <div class="card" style="width: 100%;">
+  <img src="../assets/img/carusel-2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-dark">Go somewhere</a>
+  </div>
+</div>
+        <!--fin card-->
     </div>
     <div class="col">
-      2 of 2
+      <span class="badge badge-pill badge-dark" id="videos">Videos</span>
+
+      <!--video-->
+
+      <div class="embed-responsive embed-responsive-16by9">
+           <iframe width="560" height="315" src="https://www.youtube.com/embed/Kd4KEMmn1fE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+
+      <!-- fin video-->
     </div>
   </div>
 
