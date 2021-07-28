f8149d41ab91ea3bc25c03f465ff870876ae419e
Héctor Villalobos <h.villalobosm03@gmail.com>
2021-07-28 17:04:52 -0400

    instalacion de scrollreveal

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueScrollReveal from 'vue-scroll-reveal';
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+Vue.use(VueScrollReveal);
